9a3bdf77abfbca97cb2e9fa74f22a4570abc35fe
Jacob Groß (kurtextrem) <kurtextrem@gmail.com>
2014-07-24 17:42:40 +0200

    Add clock widget

--- js/modules/clock.js
+++ js/modules/clock.js
@@ -1,17 +1,73 @@
-+function (window, $, $ajax) {
+/* global Intl */
++function (window) {
 	'use strict';
 
-	function Module () {
+	var Module = {}
 
+	Module.name = 'clock'
+
+	Module.storageKeys = [{
+		name: 'clock',
+		type: {
+			twelveHours: false
+		}
+	}]
+
+	/** @see ntp.js */
+	Module.init = function (obj) {
+		this.ui_ = new ModuleUI('#box-' + this.name, obj[this.name])
+
+		this.update()
+		window.setInterval(function () {
+			window.App.now = Date.now()
+			this.update(obj)
+		}.bind(this), 25000)
 	}
 
-	Module.prototype.name = 'clock'
-
-	Module.prototype.storageKeys = []
-
-	Module.prototype.init = function (obj) {
-
+	/** @see ntp.js */
+	Module.update = function () {
+		this.updateUI(window.App.now)
 	}
 
-	window.App.register(new Module())
-}(window, $, qwest)
+	/** @see ntp.js */
+	Module.updateUI = function (data) {
+		this.ui_.addHTML(data)
+		this._super()
+	}
+
+	/************\
+	|  UI Section   |
+	\************/
+
+	/** @see ntp.js */
+	var ModuleUI = {}
+
+	/** @see ntp.js */
+	ModuleUI.init = function (name, options) {
+		this.options = options
+		this.formatter = Intl.DateTimeFormat(window.App.lang, {
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+			weekday: 'long'
+		})
+		this._super(name)
+	}
+
+	/** @see ntp.js */
+	ModuleUI.addHTML = function (timestamp) {
+		this.html = ''
+		timestamp = new Date(timestamp)
+
+		var hours = timestamp.getHours(), postfix = this.options.twelveHours ? 'am' : ''
+		if (this.options.twelveHours && hours > 12) {
+			hours = hours - 12
+			postfix = 'pm'
+		}
+		this.html += '<span class="clock__hours">' + ('0' + hours).slice(-2) + '</span><span class="clock__minutes">' + ('0' + timestamp.getMinutes()).slice(-2) + '</span><span class="clock__postfix">' + postfix + '</span><div class="clock__date">' + this.formatter.format(timestamp) + '</div>'
+	}
+
+	ModuleUI = window.App.ModuleUI.extend(ModuleUI)
+
+	window.App.register(Module)
+}(window)
